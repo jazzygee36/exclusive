76e61f57b4214b6b9d540f3e1ca9036062408ea2
Samson Mesioye <jazzygee36gmail.com>
2024-07-15 16:18:00 +0100

    header and foooter component

---
+++ src/components/footer.tsx
@@ -0,0 +1,37 @@
+export default function Footer() {
+  return (
+    <div className='bg-[#000000] w-full h-full  pb-[24px] pt-[80px] '>
+      <div className=' flex justify-between  text-white ml-[8.5%] mr-[8.5%]'>
+        <div className='flex flex-col  gap-3 cursor-pointer'>
+          <div className='text-[14px]'>Exclusive</div>
+          <div className='text-[12px]'>Subscribe</div>
+          <div className='text-[10px]'>Get 10% off your first order</div>
+          <input
+            type='text'
+            placeholder='Enter your email'
+            className='bg-transparent border rounded-sm placeholder:text-xs'
+          />
+        </div>
+        <div className='flex flex-col  gap-3 cursor-pointer'>
+          <div className='text-[14px]'>Support</div>
+          <div className='text-[12px]'>Ikorodu Lagos. 08136668316</div>
+          <div className='text-[10px]'> exclusive@gmail.com</div>
+          <div className='text-[10px]'> 08136668316</div>
+        </div>
+        <div>
+          <h3 className='text-[14px]'>Account</h3>
+        </div>
+        <div>
+          <h3 className='text-[14px]'>Quick Link</h3>
+        </div>
+        <div>
+          <h3 className='text-[14px]'>Download App</h3>
+        </div>
+      </div>
+      <hr className='mt-[60px] ' />
+      <div className='text-[10px] text-white text-center mt-2'>
+        Copyright Rimel 2022. All right reserved
+      </div>
+    </div>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,83 @@
+export default function Header() {
+  return (
+    <div>
+      <div className='h-12 w-full bg-[#000000] flex items-center justify-center  text-[#eee] text-sm'>
+        <div className=''>
+          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+          ShopNow
+        </div>
+
+        <div className='relative left-[15%] cursor-pointer'>English</div>
+      </div>
+
+      <div className='flex justify-between items-center ml-[8.5%] mr-[8.5%] mt-[47px] mb-[27px]'>
+        <div className='font-bold text-[20px] text-[#000000]'>Exclusive</div>
+        <ul className='flex items-center gap-16 cursor-pointer'>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About</li>
+          <li>Sign Up</li>
+        </ul>
+
+        <div className='flex gap-4 items-center'>
+          <form className='max-w-md mx-auto'>
+            <div className='relative'>
+              <input
+                type='search'
+                id='default-search'
+                className='block h-8 w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                placeholder='What are you looking for?'
+              />
+              <div className='absolute end-0 inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
+                <svg
+                  className='w-4 h-4 text-gray-500 dark:text-gray-400'
+                  aria-hidden='true'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 20 20'
+                >
+                  <path
+                    stroke='currentColor'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
+                  />
+                </svg>
+              </div>
+            </div>
+          </form>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            className='size-6 cursor-pointer'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z'
+            />
+          </svg>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            className='size-6 cursor-pointer'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
+            />
+          </svg>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+}
