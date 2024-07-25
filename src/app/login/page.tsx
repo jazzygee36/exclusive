8899c4cf979e02e96a8a0bbc63365205e897409b
Samson Mesioye <jazzygee36gmail.com>
2024-07-25 12:25:57 +0100

    double quote

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,33 +9,33 @@ import { useRouter } from 'next/navigation';
 const Login = () => {
   const router = useRouter();
   return (
-    <div className="flex  gap-32 mt-[60px]">
-      <div className="w-[50%] ">
-        <Image src={Signup} alt="signup" />
+    <div className='flex  gap-32 mt-[60px]'>
+      <div className='w-[50%] '>
+        <Image src={Signup} alt='signup' />
       </div>
-      <div className="flex flex-col  w-[50%] mt-[90.5px] scroll">
-        <div className="w-[75%]">
-          <div className="font-semibold text-[36px]">Log in to Exclusive</div>
-          <div className="text-[16px] font-medium">
+      <div className='flex flex-col  w-[50%] mt-[90.5px] scroll'>
+        <div className='w-[75%]'>
+          <div className='font-semibold text-[36px]'>Log in to Exclusive</div>
+          <div className='text-[16px] font-medium'>
             Enter your details below
           </div>
 
           <HomeInput placeholder={'Email'} type={'email'} />
           <HomeInput placeholder={'Password'} type={'password'} />
         </div>
-        <div className="flex justify-between items-center mt-10 gap-16">
-          <div className="w-[100%]">
+        <div className='flex justify-between items-center mt-10 gap-16'>
+          <div className='w-[100%]'>
             <Button title={'Log in'} bg={'#DB4444'} textColor={'white'} />
           </div>
-          <div className="text-[#DB4444] font-normal text-[16px] w-[100%] ">
+          <div className='text-[#DB4444] font-normal text-[16px] w-[100%] '>
             Forget Password?
           </div>
         </div>
 
-        <div className="text-[16px] font-semibold flex  mt-8">
-          Don't have an account?
+        <div className='text-[16px] font-semibold flex  mt-8'>
+          Dont have an account?
           <div
-            className="cursor-pointer hover:underline ml-2"
+            className='cursor-pointer hover:underline ml-2'
             onClick={() => router.push('/signup')}
           >
             Sign up
